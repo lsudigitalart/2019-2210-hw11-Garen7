3075f96c7b503bc0bcc0c394b40d70f2fe71fc47
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2019-11-18 22:12:04 -0600

    done

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
+let people = []
+
 function preload(){
-   khoal = loadJSON('https://api.github.com/users/khoalitygoods')
-   janna = loadJSON('https://api.github.com/users/Janna-kelly')
+   let names = ['Aarondito', 'Ace601', 'aheb157', 'Allendm656', 'asmi564', 'cnavo1', 'fredeerock', 'gabbykraemer', 'Garen7', 'GradyCham', 'Janna-kelly', 'jlanda2', 'juanyswany', 'mbushn1', 'myorek1', 'kathrynspahn', 'khoalitygoods', 'kkolder', 'R0bobot', 'SamanthaBerkholz', 'samblaeser', 'SCguerra', 'taybutler', 'TwilightAud']
+   for(const name of names){
+      people.push(loadJSON('https://api.github.com/users/'+name))
+   }
 }
 
 function setup(){
-   createImg(khoal.avatar_url)
-   createImg(janna.avatar_url)
+   let size = 1
+   while((innerHeight-innerHeight%(size+1))*(innerWidth-innerWidth%(size+1))*(size+1)**-2 >= 24){
+      size++
+   }
+   createCanvas(0, 0)
+   let element
+   for(person of people){
+      element = createImg(person.avatar_url)
+      element.size(size, size)
+   }
 }
